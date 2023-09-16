1e531dac791a72460bf6eadc004cb5db6d92e54f
Alex Payne <apayne@esri.com>
2023-09-16 01:01:58 +0100

    example rust ed to 2021

--- f-trak-test/src/main.rs
+++ f-trak-test/src/main.rs
@@ -1,41 +1,51 @@
 extern crate f_trak;
-use std::thread;
 use std::sync::mpsc;
+use std::thread;
 
 fn main() {
    let protopath = "ADD-YOUR-FILE-PATH-TO/static/deploy.prototxt.txt".to_string();
    let modelpath = "ADD-YOUR-FILE-PATH-TO/static/model.caffemodel".to_string();
-   let min_confidence = 0.9;
 
-   type Boundingbox = ((i32, i32), (i32, i32));
+    let min_confidence = 0.9;
 
-   let (bbox_transmitter, bbox_receiver) = mpsc::channel::<Boundingbox>();
-   let (terminate_transmitter, terminate_receiver) = mpsc::channel::<bool>();
+    type Boundingbox = ((i32, i32), (i32, i32));
 
-   thread::spawn(move || {
-      println!("DEBUG: Spawned the face capture thread!");
-      let mut face_cap = f_trak::FaceCapture::new(bbox_transmitter, 
-                                                  terminate_transmitter,
-                                                  protopath,
-                                                  modelpath,
-                                                  min_confidence);
-      face_cap.begin_capture();
-   });
+    let (bbox_transmitter, bbox_receiver) = mpsc::channel::<Boundingbox>();
+    let (terminate_transmitter, terminate_receiver) = mpsc::channel::<bool>();
 
-   loop {
-      let val = bbox_receiver.try_recv();
+    thread::spawn(move || {
+        println!("DEBUG: Spawned the face capture thread!");
+        let mut face_cap = f_trak::FaceCapture::new(
+            bbox_transmitter,
+            terminate_transmitter,
+            protopath,
+            modelpath,
+            min_confidence,
+        );
+        face_cap.begin_capture();
+    });
 
-      match val {
-         Ok(t) => println!("BBOX: (({}, {}), ({}, {}))", t.0.0, t.0.1, t.1.0, t.1.1),
-         Err(e) => { println!("ERROR: {}", e); },
-      }
+    loop {
+        let val = bbox_receiver.try_recv();
 
-      let cancel = terminate_receiver.try_recv();
+        match val {
+            Ok(t) => println!("BBOX: (({}, {}), ({}, {}))", t.0 .0, t.0 .1, t.1 .0, t.1 .1),
+            Err(_e) => {
+                println!("ERROR: {}", e);
+            }
+        }
 
-      match cancel {
-         Ok(terminate_flag) => if terminate_flag {break;} ,
-         Err(e) => { println!("ERROR: {}", e); },
-      }
-   }
-   ()
+        let cancel = terminate_receiver.try_recv();
+
+        match cancel {
+            Ok(terminate_flag) => {
+                if terminate_flag {
+                    break;
+                }
+            }
+            Err(_e) => {
+                println!("ERROR: {}", e);
+            }
+        }
+    }
 }
